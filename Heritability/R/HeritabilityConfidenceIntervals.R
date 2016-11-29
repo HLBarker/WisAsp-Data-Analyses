cd6084b7d698543760014ed6d9f5a040890aaf7c
Hilary Barker <hlbultman@gmail.com>
2016-11-29 15:20:06 -0600

    Revised to fit Google's R Style Guide

--- Heritability/R/HeritabilityConfidenceIntervals.R
+++ Heritability/R/HeritabilityConfidenceIntervals.R
@@ -1,5 +1,12 @@
-# Function for 95% confidence intervals for the broad-sense heritability estimates
 confH2 <- function(lmermodel) {
+  #  Computes 95% confidence intervals for broad-sense heritability (H^2) estimates
+  #  
+  #  Args:
+  #    lmermodel = mixed effects model of class mer.mod that is used to calculate H^2
+  #      these models are made using the lmer() in the lme4 package
+  #
+  #  Returns: 
+  #    95% Confidence intervals around the H^2 estimates for a trait
   ConfVar <- (as.data.frame(confint(lmermodel, level = 0.95)^2))
   colnames(ConfVar) <- c("lower", "upper")
   H2lower <- ConfVar[1,1] / sum(ConfVar[1:3,1])
