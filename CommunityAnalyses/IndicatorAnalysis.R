52bbce06a18a30b251fd50e48a8e21f4b511b5d2
Hilary Barker <hlbultman@gmail.com>
2016-11-29 15:29:54 -0600

    Revised to fit Google's R Style Guide

--- CommunityAnalyses/IndicatorAnalysis.R
+++ CommunityAnalyses/IndicatorAnalysis.R
@@ -1,3 +1,6 @@
+# Author = Hilary Barker
+# This script identifies which species in a community (e.g., insect community) appear to respond to 
+#   environmental variables (e.g., traits of the tree that the insect community is found on)
 # Load packages ---------------------------------------------------------------
 library(labdsv)
 library(dplyr)
@@ -22,28 +25,28 @@ str(InsSp)
 
 
 # Combine data -----------------------------------------------------------------
-InsMean <- InsSp %>% group_by(GENO) %>% summarise_each(funs(mean)) #get insect means by genotype
+InsMean <- InsSp %>% group_by(GENO) %>% summarise_each(funs(mean))  # get insect means by genotype
 str(InsMean)
-TraitMean <- Trait2014 %>% group_by(GENO) %>% summarise_each(funs(mean)) #get trait means by genotype
+TraitMean <- Trait2014 %>% group_by(GENO) %>% summarise_each(funs(mean))  # get trait means by genotype
 str(TraitMean)
 
-All <- merge(TraitMean, InsMean, by.x = "GENO", by.y = "GENO") # merge dataframes 
+All <- merge(TraitMean, InsMean, by.x = "GENO", by.y = "GENO")  # merge dataframes 
 All <- na.omit(cbind(All$GENO, All[,8:30], All[,38:51]))
 str(All)
 
 # Idenfity the extreme trees  -------------------------------------------------
-  # separate the data into two groups: (1) insect communities found on trees that have extremely 
-  # high values of the trait in interest, (2) insect communities found on trees that have extremely
-  # low values of the trait in interest. Then perform species indicator analyses wich identifies
-  # the insect species that define the two different community groups and shed light on which
-  # insects are influenced by a given trait and how (do they favor high or low trait levels?)
+  #  separate the data into two groups: (1) insect communities found on trees that have extremely 
+  #  high values of the trait in interest, (2) insect communities found on trees that have extremely
+  #  low values of the trait in interest. Then perform species indicator analyses wich identifies
+  #  the insect species that define the two different community groups and shed light on which
+  #  insects are influenced by a given trait and how (do they favor high or low trait levels?)
 extremetrees <- function(trait){
   All <- All[order(trait),]
   Mostextreme <- All[86:114,25:38]  # get the most extreme insect data
   Mostextreme$Ext <- rep(2,nrow(Mostextreme))  # add on a column of ones
-  Leastextreme <- All[1:30,25:38] # get the least extreme insect data
-  Leastextreme$Ext <- rep(1,nrow(Leastextreme)) # add on a column of zeros
-  Extremes <- rbind(Leastextreme, Mostextreme) # add the two dataframes together
+  Leastextreme <- All[1:30,25:38]  # get the least extreme insect data
+  Leastextreme$Ext <- rep(1,nrow(Leastextreme))  # add on a column of zeros
+  Extremes <- rbind(Leastextreme, Mostextreme)  # add the two dataframes together
   indval(Extremes[,1:14], Extremes[,15], numitr= 1000)
 }
 
